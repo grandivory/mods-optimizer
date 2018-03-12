b752ac650c99a5e09f351390f51b637ce212ea21
Matt Butt <grandivory@gmail.com>
2018-03-11 23:20:17 -0400

    Add outline for how mod optimization will work

--- src/utils/Optimizer.js
+++ src/utils/Optimizer.js
@@ -11,7 +11,13 @@ class Optimizer {
    * @param optimizationPlan An OptimizationPlan object that gives values to all stats.
    */
   findBestModSetForCharacter(character, optimizationPlan){
-
+    // Filter out any mods that aren't usable
+    // Go through all mods and assign a value to them based on the optimization plan
+    // Assign a value to each set bonus based on the optimization plan
+    // Start with the highest-value mod in each slot
+    // Go through each set bonus with a positive value, and find the best mod sub-sets (all possible pairs or quads)
+    // Make each possible set of 6 from the sub-sets found above, including filling in with the "base" set
+    // Choose the set with the highest value
   }
 }
 
