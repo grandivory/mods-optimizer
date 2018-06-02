06451b2b5d1847744772cddf94b88ee1f0e626f5
Matt Butt <grandivory@gmail.com>
2018-06-02 11:57:07 -0400

    Make range inputs editable via direct input
    Add input fields next to the range inputs for all stat values. Moving the slider will change the value in the input field and vice-versa

--- src/containers/CharacterEditView/CharacterEditView.js
+++ src/containers/CharacterEditView/CharacterEditView.js
@@ -257,6 +257,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="health-stat">Health:</label>
           <RangeInput
+            editable={true}
             id={'health-stat'}
             name={'health-stat'}
             defaultValue={character.optimizationPlan.rawHealth}
@@ -267,6 +268,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="protection-stat">Protection:</label>
           <RangeInput
+            editable={true}
             id={'protection-stat'}
             name={'protection-stat'}
             defaultValue={character.optimizationPlan.rawProtection}
@@ -277,6 +279,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="speed-stat">Speed:</label>
           <RangeInput
+            editable={true}
             id={'speed-stat'}
             name={'speed-stat'}
             defaultValue={character.optimizationPlan.rawSpeed}
@@ -287,6 +290,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="critChance-stat">Critical Chance %:</label>
           <RangeInput
+            editable={true}
             id={'critChance-stat'}
             name={'critChance-stat'}
             defaultValue={character.optimizationPlan.rawCritChance}
@@ -297,6 +301,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="critDmg-stat">Critical Damage %:</label>
           <RangeInput
+            editable={true}
             id={'critDmg-stat'}
             name={'critDmg-stat'}
             defaultValue={character.optimizationPlan.rawCritDmg}
@@ -307,6 +312,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="potency-stat">Potency %:</label>
           <RangeInput
+            editable={true}
             id={'potency-stat'}
             name={'potency-stat'}
             defaultValue={character.optimizationPlan.rawPotency}
@@ -317,6 +323,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="tenacity-stat">Tenacity %:</label>
           <RangeInput
+            editable={true}
             id={'tenacity-stat'}
             name={'tenacity-stat'}
             defaultValue={character.optimizationPlan.rawTenacity}
@@ -327,6 +334,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="offense-stat">Offense:</label>
           <RangeInput
+            editable={true}
             id={'offense-stat'}
             name={'offense-stat'}
             defaultValue={character.optimizationPlan.rawOffense}
@@ -337,6 +345,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="defense-stat">Defense:</label>
           <RangeInput
+            editable={true}
             id={'defense-stat'}
             name={'defense-stat'}
             defaultValue={character.optimizationPlan.rawDefense}
@@ -347,6 +356,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="accuracy-stat">Accuracy:</label>
           <RangeInput
+            editable={true}
             id={'accuracy-stat'}
             name={'accuracy-stat'}
             defaultValue={character.optimizationPlan.rawAccuracy}
@@ -357,6 +367,7 @@ class CharacterEditView extends React.Component {
         <div className={'form-row'}>
           <label htmlFor="critAvoid-stat">Critical Avoidance:</label>
           <RangeInput
+            editable={true}
             id={'critAvoid-stat'}
             name={'critAvoid-stat'}
             defaultValue={character.optimizationPlan.rawCritAvoid}
