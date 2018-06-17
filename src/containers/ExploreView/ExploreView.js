c9a884bc8fc7687b934b1c769fa5f75044104006
Matt Butt <grandivory@gmail.com>
2018-06-16 22:35:45 -0400

    Squashed commit of the following:
    commit ff396ace7451e33ae4580a4f11d1db202cf6145d
    Author: Matt Butt <grandivory@gmail.com>
    Date:   Sat Jun 16 22:34:44 2018 -0400
    
        Update version
    
    commit 0558fc3bfb8511fc7060a7d684d31ae11eba9257
    Merge: f4c61c1 c7a14b8
    Author: Matt Butt <grandivory@gmail.com>
    Date:   Sat Jun 16 22:29:59 2018 -0400
    
        Merge branch 'feature/mod-filter' into develop
    
        * feature/mod-filter:
          Add a filter and sort form to the explore view that allows the user to filter mods by slot, set, primary stat, or secondary stats, and to sort by a secondary stat.
    
    commit c7a14b874a69c00009303eb69556361dfd2d5c43
    Author: Matt Butt <grandivory@gmail.com>
    Date:   Sat Jun 16 15:32:22 2018 -0400
    
        Fix linting error
    
    commit ded3a5a953e0db782989e9681d225333ae331032
    Author: Matt Butt <grandivory@gmail.com>
    Date:   Sat Jun 16 15:30:55 2018 -0400
    
        First version of Mod Filters in Explore view
    
    commit 6136ece4ad12bcba8525313bd79cb417ef2ed5cb
    Author: Matt Butt <grandivory@gmail.com>
    Date:   Thu Jun 14 16:41:10 2018 -0400
    
        Update mod filter form
    
        Add sections for filtering by slot, set, primary stat, or secondary stat, and a section for sorting by a secondary stat. Add functionality to apply the filters to the explore view.
    
    commit a37af6bb6a4593bc2617194b35a983d51da3bcd5
    Merge: 0aec6d7 156a318
    Author: Matt Butt <grandivory@gmail.com>
    Date:   Thu Jun 14 13:58:07 2018 -0400
    
        Merge branch 'develop' into feature/mod-filter
    
        * develop:
          Add padding to sets view to allow for mod popups
          Update the version
          Account for the cost of removing existing mods
          Adjust height of lists on character selection view
          Update version number
          Fix styling on character selection screen
          Add L3-37 and Young Lando Calrissian
          Fix error sorting by tag for unknown characters
          Update version
          Fix mod movement summary with unassigned mods
          Add costs to the Review screen
          Add a credits component
          Pull request feedback incorporated
          Added a summary modal to the 'Reassigning mod list'
    
    commit 0aec6d701cc5cb98a0ce5cc67a38a0092bb92ecf
    Author: Matt Butt <grandivory@gmail.com>
    Date:   Thu Jun 7 21:28:19 2018 -0400
    
        Start to mess with Mod Filters as a filter form
    
    commit 2f6edfdc6e145196c69819f8d84ac87b06eec05c
    Author: Amoliski <amoliski@gmail.com>
    Date:   Thu Jun 7 16:26:49 2018 -0500
    
        Added modFilter Component
    
    commit 20cb34382668c4db9f8290c197640bc3ed85337b
    Author: amoliski <amoliski@gmail.com>
    Date:   Thu Jun 7 10:17:47 2018 -0500
    
        Mod filter progress

--- src/containers/ExploreView/ExploreView.js
+++ src/containers/ExploreView/ExploreView.js
@@ -1,21 +1,67 @@
 import React from "react";
 import ModDetail from '../../components/ModDetail/ModDetail';
+import ModFilter from '../../components/ModFilter/ModFilter'
 
 class ExploreView extends React.Component {
-  render() {
-    const mods = this.props.mods;
 
-    const modElements = mods.map(
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      'displayedMods': props.mods
+    };
+  }
+
+  filterUpdated(filters) {
+    let displayedMods = this.props.mods
+      .filter(mod => filters.slot.includes(mod.slot))
+      .filter(mod => filters.set.includes(mod.set.name))
+      .filter(mod => filters.primary.includes(mod.primaryStat.displayType))
+      .filter(mod => mod.secondaryStats.some(stat => filters.secondary.includes(stat.displayType)));
+
+    if (filters.sort) {
+      displayedMods = displayedMods.sort((left, right) => {
+        const leftStat = left.secondaryStats.find(stat => stat.displayType === filters.sort);
+        const rightStat = right.secondaryStats.find(stat => stat.displayType === filters.sort);
+
+        const leftValue = leftStat ? leftStat.value : 0;
+        const rightValue = rightStat ? rightStat.value : 0;
+
+        return rightValue - leftValue;
+      });
+    }
+
+    this.setState({
+      'displayedMods': displayedMods
+    });
+  }
+
+  render() {
+    const modElements = this.state.displayedMods.map(
       (mod) => <ModDetail key={mod.id} mod={mod}/>
     );
 
     return (
-      <div className='mods'>
-        {modElements}
+      <div>
+        <div className={'filters'}>
+          <ModFilter
+            mods={this.props.mods}
+            onUpdate={this.filterUpdated.bind(this)}
+          />
+        </div>
+        {/*<div className="explore-view-filter">*/}
+          {/*<ModFilter*/}
+            {/*updated={this.filterUpdated.bind(this)}*/}
+            {/*ref={(filter) => {this.filter = filter;}}*/}
+          {/*/>*/}
+        {/*</div>*/}
+        <div className='mods'>
+          {modElements}
+        </div>
       </div>
     );
-
   }
+
 }
 
 export default ExploreView;
