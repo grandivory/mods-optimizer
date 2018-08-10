517e2fc5d11afd5b710bd7de990e579549ef24be
Matt Butt <grandivory@gmail.com>
2018-08-10 00:07:35 -0400

    Update version

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -17,7 +17,7 @@ import {characters} from "../../constants/characters";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.version = '1.2.2';
+    this.version = '1.2.3';
 
     this.state = {
       'view': 'optimize',
