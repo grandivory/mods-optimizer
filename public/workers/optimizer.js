c91abe7b1df9512025f6669468147cb3ff235a16
Matt Butt <grandivory@gmail.com>
2019-06-30 14:05:00 -0400

    BUG FIX: Allow selected characters to be added
    Previously, adding more characters to the selected characters list then trying to reoptimize would cause an exception to be thrown as the optimizer looked past the end of the previous run's array. Now, the optimizer will correctly optimize new characters added to the list.

--- public/workers/optimizer.js
+++ public/workers/optimizer.js
@@ -1050,6 +1050,7 @@ function optimizeMods(availableMods, characters, order, changeThreshold, lockUns
     if (
       !recalculateMods &&
       previousRun.selectedCharacters &&
+      previousRun.selectedCharacters[index] && 
       characterID === previousRun.selectedCharacters[index].id &&
       previousCharacter &&
       previousCharacter.playerValues &&
