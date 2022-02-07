40b1691c74108fbed259c6790253b0c66e8ef185
Pawee Lorsuwannarat <pawee@snowwhite.co.th>
2022-02-07 16:39:10 +0700

    Update JML target

--- src/constants/characterSettings.js
+++ src/constants/characterSettings.js
@@ -512,7 +512,7 @@ const characterSettings = {
     DamageType.special
   ),
   'GRANDMASTERLUKE': new CharacterSettings(
-    [new OptimizationPlan('PvP', 5, 20, 100, 25, 15, 0, 0, 10, 0, 0, 0, 0, 0, true)],
+    [new OptimizationPlan('PvP', 0, 25, 100, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, true)],
     ['GMLS', 'JMLS', 'GLLS'],
     DamageType.special
   ),
