eb9b4697433bdf7bbf21477986de1be4025234dd
Treemo <matthieu.chevrier@treemo.fr>
2022-10-24 19:58:56 +0200

    Added a tooltip to explain the order of priorities associated with a profile

--- src/containers/CharacterList/CharacterList.js
+++ src/containers/CharacterList/CharacterList.js
@@ -92,6 +92,7 @@ class CharacterList extends PureComponent {
     const draggable = this.props.draggable;
 
     const selectedPlan = target.name;
+    const priorityOrder = selectedPlan in defaultTargets ? defaultTargets[selectedPlan].priorityOrderStr() : null;
     const options = character.targets()
       .map(characterTarget => characterTarget.name)
       .filter(targetName => 'custom' !== targetName)
@@ -101,8 +102,8 @@ class CharacterList extends PureComponent {
           !defaultTargets[targetName].equals(
             character.optimizerSettings.targets.find(target => target.name === targetName)
           ) ? '*' : '';
-
-        return <option value={targetName} key={targetName}>{changeIndicator}{targetName}</option>;
+        const priorityOrder = defaultTargets[targetName].priorityOrderStr() || null;
+        return <option value={targetName} key={targetName} title={priorityOrder}>{changeIndicator}{targetName}</option>;
       });
 
     const onSelect = function (e) {
@@ -144,7 +145,7 @@ class CharacterList extends PureComponent {
         </div>
         <div className={'target'}>
           <label>Target:</label>
-          <Dropdown value={selectedPlan} onChange={onSelect.bind(this)}>
+          <Dropdown value={selectedPlan} onChange={onSelect.bind(this)} title={priorityOrder}>
             {options}
             <option value={'custom'}>Custom</option>
           </Dropdown>
