7d067464b71afc3aa3da07ea4df2900c521bd022
Pawee Lorsuwannarat <pawee@snowwhite.co.th>
2023-04-09 16:15:48 +0700

    Add/Update Darth Sion's targets

--- src/constants/characterSettings.js
+++ src/constants/characterSettings.js
@@ -396,8 +396,8 @@ const characterSettings = {
   ),
   'DARTHSION': new CharacterSettings(
     [
-      new OptimizationPlan('PvP', 15, 15, 100, 20, 15, 0, 20, 0, 20, 0, 0, 0, 0, true),
-      new OptimizationPlan('Tanky', 10, 20, 100, 0, 15, 0, 0, 0, 0, 10, 0, 0, 100, true)
+      new OptimizationPlan('PvP', 25, 30, 100, 0, 5, 0, 0, 0, 0, 20, 0, 0, 0, true),
+      new OptimizationPlan('PvP/CA', 25, 30, 100, 0, 5, 0, 0, 0, 0, 20, 0, 0, 100, true)
     ],
     ['Nightmare']
   ),
